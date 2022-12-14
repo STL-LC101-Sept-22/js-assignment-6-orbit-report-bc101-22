7187dc0942aec81e1d47369e85ca6e0ed3f7effe
bc101-22 <chensi2021@gmail.com>
2022-12-14 04:31:33 -0500

    bonus

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -2,30 +2,66 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Satellite } from '../satellite';
 
 @Component({
-  selector: 'app-orbit-counts',
-  templateUrl: './orbit-counts.component.html',
-  styleUrls: ['./orbit-counts.component.css']
+	selector: 'app-orbit-counts',
+	templateUrl: './orbit-counts.component.html',
+	styleUrls: ['./orbit-counts.component.css']
 })
 export class OrbitCountsComponent implements OnInit {
 
 	@Input() satellites: Satellite[];
 
-  constructor() { }
+	types: string[] = [
+		"Space Debris",
+		"Communication",
+		"Probe",
+		"Positioning",
+		"Space Station",
+		"Telescope"
+	]
+	
+	// typeCount: object[] = [
+	// 	{
+	// 		type: "Space Debris",
+	// 		count: 0
+	// 	},
+	// 	{
+	// 		type: "Communication",
+	// 		count: 0
+	// 	},
+	// 	{
+	// 		type: "Probe",
+	// 		count: 0
+	// 	},
+	// 	{
+	// 		type: "Positioning",
+	// 		count: 0
+	// 	},
+	// 	{
+	// 		type: "Space Station",
+	// 		count: 0
+	// 	},
+	// 	{
+	// 		type: "Telescope",
+	// 		count: 0
+	// 	}
+	// ]
 
-  ngOnInit() {
-  }
+	constructor() { }
 
-  countByType(type: string): number {
-	let count = 0;
-	if (this.satellites) {
-	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
-			count++;
-		 }
-	  }
+	ngOnInit() {
 	}
-	return count;
- }
+
+	countByType(type: string): number {
+		let count = 0;
+		if (this.satellites) {
+			for (let i = 0; i < this.satellites.length; i++) {
+				if (this.satellites[i].type === type) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
 
 
 }
